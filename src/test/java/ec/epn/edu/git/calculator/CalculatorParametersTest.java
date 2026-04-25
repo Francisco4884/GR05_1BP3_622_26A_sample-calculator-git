f34c30b36c43feb8888af0b259337f3203776f40
xjavi3r <brand0javi3r@hotmail.com>
2026-04-25 01:41:38 -0500

    Prueba por paramétros

---
+++ src/test/java/ec/epn/edu/git/calculator/CalculatorParametersTest.java
@@ -0,0 +1,43 @@
+package ec.epn.edu.git.calculator;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(value = Parameterized.class)
+public class CalculatorParametersTest {
+
+    private int a;
+    private int b;
+    private int expected;
+
+    // El constructor recibe los parámetros de cada fila de la lista
+    public CalculatorParametersTest(int a, int b, int expected) {
+        this.a = a;
+        this.b = b;
+        this.expected = expected;
+    }
+
+    // Definición del conjunto de datos para la prueba
+    @Parameterized.Parameters
+    public static Iterable<Object[]> parameters() {
+        List<Object[]> objects = new ArrayList<Object[]>();
+        objects.add(new Object[]{2, 4, 6});
+        objects.add(new Object[]{1, 8, 9});
+        objects.add(new Object[]{2, 5, 7});
+        objects.add(new Object[]{7, 3, 10});
+        objects.add(new Object[]{22, 4, 26});
+        objects.add(new Object[]{12, 14, 26});
+        return objects;
+    }
+
+    @Test
+    public void given_parameters_when_addition_then_summation() {
+        // Se asume que existe una clase llamada Calculator con un método addition
+        Calculator c = new Calculator();
+        int actual = c.sumar(a, b);
+        assertEquals(expected, actual);
+    }
+}
