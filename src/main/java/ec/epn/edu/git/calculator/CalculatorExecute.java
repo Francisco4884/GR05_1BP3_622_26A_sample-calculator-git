0f3e2f761f12813489ea20cce48e7974b62f5d02
JoseDA0721 <133171655+JoseDA0721@users.noreply.github.com>
2026-04-24 20:52:15 -0500

    Adding calculator execute. Adding calculator java archive JAR.

---
+++ src/main/java/ec/epn/edu/git/calculator/CalculatorExecute.java
@@ -0,0 +1,13 @@
+package ec.epn.edu.git.calculator;
+
+public class CalculatorExecute {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a = 10;
+        int b = 5;
+        System.out.println("Suma (10 + 5): " + calculator.sumar(a, b));
+        System.out.println("Resta (10 - 5): " + calculator.restar(a, b));
+        System.out.println("Multiplicación (10 * 5): " + calculator.multiplicar(a, b));
+        System.out.println("División (10 / 5): " + calculator.dividir(a, b));
+    }
+}
